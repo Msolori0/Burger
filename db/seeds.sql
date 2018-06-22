56e75acb78e475dcd3af096b8fec1f63561a2f27
Solorio <solorio_michelle@yahoo.com>
2018-06-22 16:37:28 -0400

    adding inital files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("South Western Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("All American Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES("Veggie Burger", FALSE);
+
+SELECT * FROM burgers;
