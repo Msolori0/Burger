3e0945d0deb22892e06607eda897e045c2e43b26
Solorio <solorio_michelle@yahoo.com>
2018-06-23 23:18:19 -0400

    initial upload

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
