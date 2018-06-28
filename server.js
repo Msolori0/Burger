accbcc452f54ad7c5f8979edcc199d727f4e2310
Solorio <solorio_michelle@yahoo.com>
2018-06-27 23:40:06 -0400

    mostly finished, still having some issues

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override')
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/burger_controllers.js');
+app.use('/', router);
 
 app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
